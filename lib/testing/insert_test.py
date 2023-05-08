c9971b446c4787d925b675eba52f5f04f538b2c6
Bruce <bruce.hillsworth@gmail.com>
2023-05-07 22:12:51 -0600

    Passed all tests

--- lib/testing/insert_test.py
+++ lib/testing/insert_test.py
@@ -14,16 +14,16 @@ insert_file = open("lib/insert.sql")
 insert_as_string = insert_file.read()
 cursor.executescript(insert_as_string)
 
+
 class TestInsert:
-    '''Statement in insert.sql'''
+    """Statement in insert.sql"""
 
     def test_inserts_eight_bears_into_table(self):
-        '''inserts 8 bears into bears table.'''
+        """inserts 8 bears into bears table."""
         result = cursor.execute("SELECT COUNT(*) FROM bears;")
-        assert(result.fetchall()[0][0] == 8)
+        assert result.fetchall()[0][0] == 8
 
     def test_has_unnamed_bear(self):
-        '''inserts one unnamed bear into bears table.'''
+        """inserts one unnamed bear into bears table."""
         result = cursor.execute("SELECT COUNT(*) FROM bears WHERE name IS NULL;")
-        assert(result.fetchall()[0][0] == 1)
-    
+        assert result.fetchall()[0][0] == 1
